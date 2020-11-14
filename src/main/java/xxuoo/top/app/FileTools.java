d89fb562c659a1a1a749342d2f7a600ba4dfa196
MoasicVirus <mr.zjx1992@163.com>
2020-11-15 01:22:37 +0800

    更新为pom工程

---
+++ src/main/java/xxuoo/top/app/FileTools.java
@@ -0,0 +1,5 @@
+package xxuoo.top.app;
+
+public class FileTools {
+
+}
---
+++ src/main/java/xxuoo/top/app/Main.java
@@ -0,0 +1,17 @@
+package xxuoo.top.app;
+
+public class Main {
+	
+
+	// 源文件路径
+	static String origFinderPath = "E://04-照片//02-新的照片";
+	// 输出文件路径
+	static String restFinderPath = "E://00-处理结果//";
+	// 需要获取的文件类型
+	static String[] fileType = {"jpg","jpeg","png","gif","mov","mp4","avi","3gp","wmv"};
+
+
+	public static void main(String[] args) {
+		
+	}
+}
